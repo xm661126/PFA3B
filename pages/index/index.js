5f597fd58c30457845f78f7ab34bc2e3c638f563
xm661126 <xm632866568@163.com>
2019-11-22 15:28:05 +0800

    V1.2

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,35 +11,49 @@ const cityMap = {
   'qt': '其他',
 }
 Page({
+  data: {
+    
+
+  },
+  onLoad() {
+    wx.setNavigationBarTitle({
+      title: '最热新闻news',
+    })
+    wx.setNavigationBarColor({
+
+      frontColor: '#ffffff',
+      backgroundColor: '#1BC3B8',
+    })
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
       data: {
-
-
+        type: "gn"
       },
-      onLoad() {
-        wx.setNavigationBarTitle({
-          title: '最热新闻news',
-        })
-        wx.setNavigationBarColor({
-
-          frontColor: '#ffffff',
-          backgroundColor: '#1BC3B8',
-        })
-      },
-
-      getInformation() {
-        wx.showToast({
-            title: '正在链接',
-          })
-          // wx.request({
-          //   url: 'https://test-miniprogram.com/api/news/list',
-          //   data:{
-          //     type:"gn"
-          //   },
-          //   success: res => {
-          //     console.log(res.data)
-          //     let result = res.data.result
-          //     let temp = result.now.temp
-          //     let weather = result.now.weather
-          //   }
+      success: res => {
+        console.log(res);
+        // let result = res.data.result
+        // let nowTitle = result[0]  
+        // let nowDate = result.source
+        // let nowFirstImage=result.firstImage
+        // onsole.log(''，nowFirstImage)
       }
+    })
+  },
+
+  getInformation() {
+    wx.showToast({
+      title: '正在链接',
+    })
+    // wx.request({
+    //   url: 'https://test-miniprogram.com/api/news/list',
+    //   data:{
+    //     type:"gn"
+    //   },
+    //   success: res => {
+    //     console.log(res.data)
+    //     let result = res.data.result
+    //     let temp = result.now.temp
+    //     let weather = result.now.weather
+    //   }
+  }
 })
