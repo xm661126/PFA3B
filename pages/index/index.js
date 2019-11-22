8b4b84cd243a31a35ebc541a31a51769f048c0e7
xm661126 <xm632866568@163.com>
2019-11-22 13:50:34 +0800

    V1.1
    开始尝试使用API

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,18 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const cityMap = {
+  'gn': '国内',
+  'gj': '国际',
+  'cj': '财经',
+  'yl': '娱乐',
+  'js': '军事',
+  'ty': '体育',
+  'qt': '其他',
+}
 Page({
   data:{
+    
 
   },
   onLoad(){
@@ -16,6 +25,8 @@ Page({
       backgroundColor: '#1BC3B8',
     })
   },
+  
+ 
   
 
 })
