c21cece1d655adaa8f3e672a406353f16eec1f4b
xm661126 <xm632866568@163.com>
2019-11-26 15:06:18 +0800

    1.9
    完成版，待美化

--- pages/list/list.js
+++ pages/list/list.js
@@ -9,9 +9,17 @@ Page({
   data: {
     listtype: "我爱吃肉",
     id: "", 
-    newsResult2:[]
+    newsResult2:[],
+    temp:'',
   },
   onLoad() {
+    wx.setNavigationBarTitle({
+      title: '最热新闻news',
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: '#1BC3B8',
+    })
     let id=""
     let listtype = app.globalData.globaltype
     console.log(listtype,'get new value')
@@ -26,12 +34,14 @@ Page({
       },
       success: res => {
         let newsResult2=[];
+        let temp="p";
         console.log(res);
         console.log(id, "get type information");
         newsResult2 = res.data.result;
         this.setData({
           // origindata1: origindata1,
-          newsResult2: newsResult2
+          newsResult2: newsResult2,
+          temp:temp
         })
         console.log(newsResult2);
       }
