b8caf41dd3bfe872d3bef351d66a1ba09bd45539
xm661126 <xm632866568@163.com>
2019-11-26 16:44:28 +0800

    v2.0z最终版
    网上抄了几段代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -97,6 +97,33 @@ Page({
 
     //我是辅助线
   },
+  // 获取滚动条当前位置
+  onPageScroll: function (e) {
+    console.log(e)
+    if (e.scrollTop > 100) {
+      this.setData({
+        floorstatus: true
+      });
+    } else {
+      this.setData({
+        floorstatus: false
+      });
+    }
+  },
+
+  //回到顶部
+  goTop: function (e) {  // 一键回到顶部
+    if (wx.pageScrollTo) {
+      wx.pageScrollTo({
+        scrollTop: 0
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
+  },
   onTapDayNews(e) {
   wx.showToast({
     title: '正在为您跳转页面',
